a06f8f1751a0dcb8bdff12dc224bc8f6a2f6ddc8
chelsepit <cf.maranga@gmail.com>
2025-10-25 17:00:33 +0800

    FIX: for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    build: {
+        outDir: "public/build", 
+        manifest: true,        
+        rollupOptions: {
+            input: ["resources/css/app.css", "resources/js/app.js"],
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
